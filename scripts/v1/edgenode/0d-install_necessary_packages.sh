514723da13ed90188154f0e3eab84c60f437f266
EndeavorYen <endeavorisforever@gmail.com>
2019-12-20 18:12:43 +0800

    Added New Script 0e.

--- scripts/v1/edgenode/0d-install_necessary_packages.sh
+++ scripts/v1/edgenode/0d-install_necessary_packages.sh
@@ -2,7 +2,7 @@
 
 ENV=EdgeNode
 
-if [ "$( hostname )" != "edgenod" ]; then echo; echo ">>> Plz Execute this Script on $ENV <<<"; echo; exit; fi
+if [ "$( hostname )" != "edgenode" ]; then echo; echo ">>> Plz Execute this Script on $ENV <<<"; echo; exit; fi
 
 # --------------------------------------
 
