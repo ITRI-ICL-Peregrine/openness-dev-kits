3bc941d4e768cae9b32b0b574ba55861ada0120c
EndeavorYen <endeavorisforever@gmail.com>
2019-12-24 21:30:52 +0800

    Fixed Bugs

--- v1/deploynode/1b-import-vm-config.sh
+++ v1/deploynode/1b-import-vm-config.sh
@@ -11,19 +11,22 @@ echo; echo "Change Hostname - Controller"; echo
 
 IMAGE=Controller.qcow2
 
-cd $WORKING_DIR/img
+#cd $WORKING_DIR/img
 MOUT_DIR=$WORKING_DIR/img/tmp
 mkdir -p $MOUT_DIR
-guestmount -a $IMAGE  -m /dev/centos/root $MOUT_DIR
+guestmount -a $WORKING_DIR/img/$IMAGE  -m /dev/centos/root $MOUT_DIR
 
 mount --bind /dev $MOUT_DIR/dev
 mount --bind /proc $MOUT_DIR/proc
 mount --bind /sys $MOUT_DIR/sys
 
 #Change hostname
-chroot $MOUT_DIR echo "Controller" > /etc/hostname
-chroot $MOUT_DIR echo "127.0.0.1 Controller" >> /etc/hosts
-chroot $MOUT_DIR echo "::1 Controller" >> /etc/hosts
+chroot $MOUT_DIR /bin/bash << "EOT"
+echo "Controller" > /etc/hostname
+echo "127.0.0.1 Controller" >> /etc/hosts
+echo "::1 Controller" >> /etc/hosts
+EOT
+
 
 #Unmount
 umount $MOUT_DIR/dev
@@ -37,19 +40,21 @@ echo; echo "Change Hostname - EdgeNode"; echo
 
 IMAGE=EdgeNode.qcow2
 
-cd $WORKING_DIR/img
+#cd $WORKING_DIR/img
 MOUT_DIR=$WORKING_DIR/img/tmp
 mkdir -p $MOUT_DIR
-guestmount -a $IMAGE  -m /dev/centos/root $MOUT_DIR
+guestmount -a $WORKING_DIR/img/$IMAGE  -m /dev/centos/root $MOUT_DIR
 
 mount --bind /dev $MOUT_DIR/dev
 mount --bind /proc $MOUT_DIR/proc
 mount --bind /sys $MOUT_DIR/sys
 
 #Change hostname
-chroot $MOUT_DIR echo "EdgeNode" > /etc/hostname
-chroot $MOUT_DIR echo "127.0.0.1 EdgeNode" >> /etc/hosts
-chroot $MOUT_DIR echo "::1 EdgeNode" >> /etc/hosts
+chroot $MOUT_DIR /bin/bash << "EOT" 
+echo "EdgeNode" > /etc/hostname
+echo "127.0.0.1 EdgeNode" >> /etc/hosts
+echo "::1 EdgeNode" >> /etc/hosts
+EOT
 
 #Unmount
 umount $MOUT_DIR/dev
